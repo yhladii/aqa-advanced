a8e0baa887e0fc1d0d9553fe8a4a9581bb6cccc1
Yuliia Hladii <yuliia.hladii@trinetix.com>
2025-10-18 19:39:57 +0200

    HW3

---
+++ jsBasics/task2.js
@@ -0,0 +1,10 @@
+let firstName = "Yuliia";
+let secondName = "Ira";
+
+//with + 
+let greetingOne = "Hello" + " " + firstName + " " + "and" + " " + secondName + "!";
+console.log(greetingOne);
+
+//ahsblon string
+let greetingTwo = `Hello ${firstName} and ${secondName}!`;
+console.log(greetingTwo);
