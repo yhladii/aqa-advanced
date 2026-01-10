cd74b60db7ba4c4f2bd702f5b24b144d62b4d483
Yuliia Hladii <yu.hladii@gmail.com>
2026-01-10 13:58:42 +0100

    Homework3.Intro to JS.Node basic and chalk examples

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Hello world!'));
+console.log(chalk.blue.bgRed.bold('Homework3 aqa advanced course'));
+console.log(chalk.red.underline('Random red text'));
