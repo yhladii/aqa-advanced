80bb39cdce22701095f4ec31369bb9ead961dc9a
Yuliia Hladii <yu.hladii@gmail.com>
2026-01-15 21:20:39 +0100

    Homework4.JavaScript primitive data types

---
+++ jsBasics/task2.js
@@ -0,0 +1,6 @@
+let name1 = "Yulia";
+let name2 = "Kate";
+let greeting1 = "Hello" + " " + name1 + " " +  "and" + " " + name2 + "!Welcome to home!"
+let greeting2 = `Hello ${name1} and ${name2}!Welcome to home!`
+console.log(greeting1) 
+console.log(greeting2)
