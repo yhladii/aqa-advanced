78cb5a1980c6d43106e5fd2183ea034f801e5736
Yuliia Hladii <yu.hladii@gmail.com>
2026-02-11 20:20:38 +0100

    Homework12

--- functions/task1.js
+++ functions/task1.js
@@ -17,3 +17,9 @@ console.log('Function expression.Rectangle Area:', rectangleAreaExpr(5, 10));
 
 const rectangleAreaArr = (width, height) => width * height;
 console.log('Arrow function.Rectangle Area:', rectangleAreaArr(5, 10));
+
+
+function foo() {
+  const x = 42; // <- unused var
+}
+foo();
